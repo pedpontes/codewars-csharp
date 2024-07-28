9a89f3aa9172e79bf5f49af651217269cf294d93
unknown <pontespeter@gmail.com>
2024-07-28 13:37:16 -0300

    codewars ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ex1
+{
+    public class Program{
+        public static void Main(string[] args){
+            string result = Kata.SpinWords("Erro no dicionario");
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Kata{
+        public static string SpinWords(string sentence){
+            string[] words = sentence.Split(" ");
+            for(int i = 0; i < words.Length; i++){
+                words[i] = words[i].Length >= 5 ? new string(words[i].Reverse().ToArray()) : words[i];
+            }
+            return string.Join(" ", words);
+        }
+    }
+}
