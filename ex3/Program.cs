0e84014840986ec0f7f9807bbfcc2a20120fd636
unknown <pontespeter@gmail.com>
2024-07-30 20:38:51 -0300

    add exercises kata codewars

---
+++ ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+namespace ex3
+{
+    public class Kata{
+        public static void Main(string[] args){
+
+            Console.WriteLine(Smash(["hello", "world", "this", "is", "great"]));
+        }
+
+        public static string Smash(string[] words){
+            return string.Join(" ", words).Trim();
+        }
+    }
+
+}
---
+++ ex4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ex4
+{
+    public class Kata{
+        public static void Main(string[] args){
+            int[] x = Maps([1, 2, 3]);
+            foreach (int number in x)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public static int[] Maps(int[] x){
+            x = x.Select(x => x*2).ToArray();
+            return x;
+        }
+    }
+}
