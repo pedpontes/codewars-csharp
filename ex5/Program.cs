313aa3a7b942044fb7ced89a81a6e3d71989c1cf
unknown <pontespeter@gmail.com>
2024-07-31 17:51:48 -0300

    ex5 list alphabet

---
+++ ex5/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+namespace ex5
+{
+    public class Program{
+        public static void Main(string[] args){
+            ListAlphabet(10);
+        }
+        public static void ListAlphabet(int num){
+            int index = 0;
+            char[] chars = new char[num];
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+            foreach (char c in alpha){
+                if(index < chars.Length) chars[index++] = c;
+            }
+            foreach (char c in chars){
+                Console.Write(c + " ");
+            }
+        }
+    }
+}
