243d4eb8608942a9ad6fa30597137e5a29774365
unknown <pontespeter@gmail.com>
2024-07-31 22:16:11 -0300

    ex5 ex6

---
+++ ex6/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+namespace ex6
+{
+    public class Kata{
+        public static void Main(string[] args){
+            Console.WriteLine(ZeroFuel(50,25,2));
+            Console.WriteLine(ZeroFuel(100,50,1));
+        }
+        public static bool ZeroFuel(uint distanceToPump, uint mpg, uint fuelLeft){
+            return distanceToPump <= mpg * fuelLeft;
+        }
+    }
+}
