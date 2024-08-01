987f0a2f15710bf711acb2ca9a1bce2e19155907
unknown <pontespeter@gmail.com>
2024-08-01 20:27:47 -0300

    ex7 ex8

--- ex7/Program.cs
+++ ex7/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ex7
+{
+    public class Kata{
+        public static void Main(string[] args){
+            Console.WriteLine(GetGrade(95,90,93));
+            Console.WriteLine(GetGrade(70,70,100));
+            Console.WriteLine(GetGrade(70,71,72));
+            Console.WriteLine(GetGrade(32,15,21));
+            Console.WriteLine(GetGrade(65,66,60));
+        }
+        public static char GetGrade(int s1, int s2, int s3){
+            int mean = (s1+s2+s3)/3;
+            if(mean < 60) return 'F';
+            else if(mean < 70) return 'D';
+            else if(mean < 80) return 'C';
+            else if(mean < 90) return 'B';
+            else return 'A';
+        }
+    }
+}
---
+++ ex8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace ex8
+{
+    public class Kata{
+        public static void Main(string[] args){
+            Console.WriteLine(BetterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 50));
+        }
+        public static bool BetterThanAverage(int[] ClassPoints, int YourPoints){
+            return YourPoints > ClassPoints.Average();
+        }
+    }
+}
