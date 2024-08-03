08a71603abaa878279e37290ad0987d9b355ff5b
unknown <pontespeter@gmail.com>
2024-08-02 23:08:38 -0300

    dictionary class

---
+++ ex9/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ex9
+{
+    public class Kata{
+        public static void Main(string[] args){
+
+            foreach (var item in Count("hoje em dia voce sabe"))
+            {
+                Console.WriteLine($"{item.Key} {item.Value}");
+            }
+        }
+        public static Dictionary<char, int> Count(string str){
+            Dictionary<char,int> strCount = new Dictionary<char, int>();
+            foreach(char c in str){
+                if(strCount.ContainsKey(c)) strCount[c]++;
+                else    strCount.Add(c,1);
+            }
+            return strCount;
+        }
+    }
+}
